de55e55dae3cfcaae08cd1a3290e8a6c2e1e779f
Leuri <leuri77@gmail.com>
2022-11-18 12:22:47 +0100

    Api config done

---
+++ DealershipProject/DealershipProject/Helpers/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DealershipProject.Helpers
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("dealershipDatabase"));
+        }
+
+        //public DbSet<Owners> Users { get; set; }
+        //public DbSet<Vehicles> Users { get; set; }
+        //public DbSet<Claims> Users { get; set; }
+    }
+}
